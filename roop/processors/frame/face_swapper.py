9280cac88aea549509f09114a457b35ad089cda3
DUMMYONFIRE <89653048+DUMMYONFIRE@users.noreply.github.com>
2023-12-19 07:46:59 +0530

    Update face_swapper.py

--- roop/processors/frame/face_swapper.py
+++ roop/processors/frame/face_swapper.py
@@ -1,4 +1,4 @@
-from typing import List, Callable
+from typing import Any, List, Callable
 import cv2
 import insightface
 import threading
@@ -34,27 +34,20 @@ def clear_face_swapper() -> None:
 
 def pre_check() -> bool:
     download_directory_path = resolve_relative_path('../models')
-    conditional_download(download_directory_path, ['https://huggingface.co/datasets/OwlMaster/gg2/resolve/main/inswapper_128.onnx'])
+    conditional_download(download_directory_path, ['https://huggingface.co/CountFloyd/deepfake/resolve/main/inswapper_128.onnx'])
     return True
 
 
 def pre_start() -> bool:
-    source_path = roop.globals.source_path
-    target_path = roop.globals.target_path
-
-    if not is_image(source_path):
-        update_status('Select an image for the source path.', NAME)
+    if not is_image(roop.globals.source_path):
+        update_status('Select an image for source path.', NAME)
         return False
-
-    source_face_image = cv2.imread(source_path)
-    if not get_one_face(source_face_image):
-        update_status('No face in the source path detected.', NAME)
+    elif not get_one_face(cv2.imread(roop.globals.source_path)):
+        update_status('No face in source path detected.', NAME)
         return False
-
-    if not is_image(target_path) and not is_video(target_path):
-        update_status('Select an image or video for the target path.', NAME)
+    if not is_image(roop.globals.target_path) and not is_video(roop.globals.target_path):
+        update_status('Select an image or video for target path.', NAME)
         return False
-
     return True
 
 
@@ -68,19 +61,21 @@ def swap_face(source_face: Face, target_face: Face, temp_frame: Frame) -> Frame:
 
 
 def process_frame(source_face: Face, reference_face: Face, temp_frame: Frame) -> Frame:
-    many_faces = get_many_faces(temp_frame) if roop.globals.many_faces else []
-    target_faces = many_faces or [find_similar_face(temp_frame, reference_face)].filter(None)
-
-    for target_face in target_faces:
-        temp_frame = swap_face(source_face, target_face, temp_frame)
-
+    if roop.globals.many_faces:
+        many_faces = get_many_faces(temp_frame)
+        if many_faces:
+            for target_face in many_faces:
+                temp_frame = swap_face(source_face, target_face, temp_frame)
+    else:
+        target_face = find_similar_face(temp_frame, reference_face)
+        if target_face:
+            temp_frame = swap_face(source_face, target_face, temp_frame)
     return temp_frame
 
 
 def process_frames(source_path: str, temp_frame_paths: List[str], update: Callable[[], None]) -> None:
     source_face = get_one_face(cv2.imread(source_path))
     reference_face = None if roop.globals.many_faces else get_face_reference()
-
     for temp_frame_path in temp_frame_paths:
         temp_frame = cv2.imread(temp_frame_path)
         result = process_frame(source_face, reference_face, temp_frame)
@@ -102,5 +97,4 @@ def process_video(source_path: str, temp_frame_paths: List[str]) -> None:
         reference_frame = cv2.imread(temp_frame_paths[roop.globals.reference_frame_number])
         reference_face = get_one_face(reference_frame, roop.globals.reference_face_position)
         set_face_reference(reference_face)
-
     roop.processors.frame.core.process_video(source_path, temp_frame_paths, process_frames)
